34b04da4d1487e48189d0e32ea65d1e4c9d52038
Greg Glockner <greg@glockners.net>
2021-02-10 20:36:12 -0800

    Update teslajson.py
    Refactor with objects for both kinds of sessions

--- teslajson.py
+++ teslajson.py
@@ -20,14 +20,15 @@ import random
 import base64
 import hashlib
 import re
-from oauthlib.oauth2 import BackendApplicationClient
+import oauthlib.oauth2
 
 
-class Connection(requests_oauthlib.OAuth2Session):
+class Connection(object):
     """Connection to Tesla Motors API"""
     def __init__(self,
             email='',
-            password=''):
+            password='',
+            **kwargs):
         """Initialize connection object
         
         Sets the vehicles field, a list of Vehicle objects
@@ -38,30 +39,33 @@ class Connection(requests_oauthlib.OAuth2Session):
         password: your password for teslamotors.com
         """
         
-        self.auth_uri = "https://auth.tesla.com"
-        self.base_uri = "https://owner-api.teslamotors.com"
-        self.data_uri = self.base_uri + "/api/1/"
+        self.email = email
+        self.password = password
+        self.sso_uri = "https://auth.tesla.com"
+        self.api_uri = "https://owner-api.teslamotors.com"
+        self.data_uri = self.api_uri + "/api/1/"
+        self.api_client_id = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
+        self.api_client_secret = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
 
-        self.get_sso_token(email, password)
-        self.fetch_token()
+        self.fetch_token(**kwargs)
 
         # Get vehicles
         self.vehicles = [Vehicle(v, self) for v in self.getdata('vehicles')['response']]
-            
 
-    def get_sso_token(self, email, password):
-        redirect_uri = self.auth_uri + "/void/callback"
+    def fetch_sso_token(self, **kwargs):
+        redirect_uri = self.sso_uri + "/void/callback"
 
         # Step 1: Obtain the login page
-        auth_sess = requests_oauthlib.OAuth2Session(
+        self.sso_session = requests_oauthlib.OAuth2Session(
           redirect_uri = redirect_uri,
-          client_id='ownerapi')
+          client_id='ownerapi',
+          **kwargs)
         self.__randchars = string.ascii_lowercase+string.digits
         code_verifier = self.__randstr(86)
         hexdigest = hashlib.sha256(code_verifier.encode('utf-8')).hexdigest()
         code_challenge = base64.urlsafe_b64encode(hexdigest.encode('utf-8')).decode('utf-8')
-        login_uri = self.auth_uri+'/oauth2/v3/authorize'
-        auth_sess.params = {
+        login_uri = self.sso_uri+'/oauth2/v3/authorize'
+        self.sso_session.params = {
           'client_id': 'ownerapi',
           'code_challenge': code_challenge,
           'code_challenge_method': 'S256',
@@ -69,53 +73,62 @@ class Connection(requests_oauthlib.OAuth2Session):
           'response_type': 'code',
           'scope': 'openid email offline_access',
           'state': self.__randstr(24) }
-        r = auth_sess.get(login_uri)
+        r = self.sso_session.get(login_uri)
         r.raise_for_status()
         login_data = dict(re.findall(
           '<input type="hidden" name="([^"]*)" value="([^"]*)"', r.text))
 
         # Step 2: Obtain an authorization code
-        login_data['identity'] = email
-        login_data['credential'] = password
-        r = auth_sess.post(login_uri, data=login_data, allow_redirects=False)
+        login_data['identity'] = self.email
+        login_data['credential'] = self.password
+        r = self.sso_session.post(login_uri, data=login_data, allow_redirects=False)
         r.raise_for_status()
         m = re.search('code=([^&]*)',r.headers['location'])
         authorization_code = m.group(1)
         
         # Step 3: Exchange authorization code for bearer token
-        auth_sess.params = None
-        self.sso_token = auth_sess.fetch_token(self.auth_uri+'/oauth2/v3/token',
+        self.sso_session.params = None
+        self.sso_session.token_url = self.sso_uri+'/oauth2/v3/token'
+        self.sso_session.fetch_token(
+            self.sso_session.token_url,
             code=authorization_code, code_verifier=code_verifier,
             include_client_id=True)
       
-    def fetch_token(self,
-        client_id = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384",
-        client_secret = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"):
-
+    def fetch_api_token(self, **kwargs):
         # Step 4: Exchange bearer token for access token
         # (Create the main oauth2 session by calling the super initializer)
-        client = BackendApplicationClient(client_id=client_id, token_type='Bearer')
+        client = oauthlib.oauth2.BackendApplicationClient(
+          client_id=self.api_client_id, token_type='Bearer')
         client.grant_type = 'urn:ietf:params:oauth:grant-type:jwt-bearer'
-        super().__init__(client=client)
-        super().fetch_token(token_url=self.base_uri+'/oauth/token',
-          client_secret=client_secret,
-          headers={'Authorization': 'Bearer %s' % self.sso_token['access_token']},
+        self.api_session = requests_oauthlib.OAuth2Session(client=client, **kwargs)
+        self.api_session.token_url = self.api_uri+'/oauth/token'
+        self.api_session.fetch_token(
+          self.api_session.token_url,
+          client_secret=self.api_client_secret,
+          headers={'Authorization': 'Bearer %s' % self.sso_session.token['access_token']},
           include_client_id=True)
     
-    # TODO: refresh_token
+    def fetch_token(self, **kwargs):
+        # Fetch both tokens
+        self.fetch_sso_token(**kwargs)
+        self.fetch_api_token(**kwargs)
+
+    def refresh_token(self):
+        # Note: We only need to refresh the API token
+        self.api_session.refresh_token(self.api_session.token_url)
     
     def __randstr(self, n):
         return ''.join(random.choice(self.__randchars) for i in range(n))
     
     def getdata(self, command):
         """Utility command to get data from API"""
-        r = self.get(self.data_uri + command)
+        r = self.api_session.get(self.data_uri + command)
         r.raise_for_status()
         return r.json()
 
     def postdata(self, command, data={}):
         """Utility command to post data to API"""
-        r = self.post(self.data_uri + command, data)
+        r = self.api_session.post(self.data_uri + command, data)
         r.raise_for_status()
         return r.json()['response']
         
